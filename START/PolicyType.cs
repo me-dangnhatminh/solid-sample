74f64208f7990c4fddbb93603162204d41a4e84d
Dang Nhat Minh <dangnhatminh09032002@gmail.com>
2022-07-25 02:00:20 +0700

    start

---
+++ START/PolicyType.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Rating
+{
+    public enum PolicyType
+    {
+        Life = 0,
+        Land = 1,
+        Auto = 2
+    }
+}
---
+++ START/Program.cs
@@ -0,0 +1,15 @@
+﻿using Rating;
+
+Console.WriteLine("Hệ thống xếp hạng đang bắt đầu..."); // Logging
+
+var engine = new RatingEngine();
+engine.Rate();
+
+if (engine.Rating > 0)
+{
+    Console.WriteLine($"Xếp hạng: {engine.Rating}");
+}
+else
+{
+    Console.WriteLine("Không có sản phẩm xếp hạng.");
+}
